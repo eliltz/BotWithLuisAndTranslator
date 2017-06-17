2712d0e65fb7420873955f08617f4c2e5a1625d0
Eli Tz <eliltz@gmail.com>
2017-06-17 20:45:50 +0300

    Add project files.

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,131 @@
+﻿namespace LuisBot
+{
+    using System;
+    using System.Diagnostics;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Configuration;
+    using System.Web.Http;
+    using Dialogs;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+    using Services;
+    using System.Net.Http.Headers;
+    using System.Xml.Linq;
+
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        private static readonly bool IsSpellCorrectionEnabled = bool.Parse(WebConfigurationManager.AppSettings["IsSpellCorrectionEnabled"]);
+
+        private readonly BingSpellCheckService spellService = new BingSpellCheckService();
+        string ApiKey = "e2e6074acad9425999d89ba796c85275";
+        string targetLang = "en";
+
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+              //  if (IsSpellCorrectionEnabled)
+               // {
+                    try
+                    {
+                        //  activity.Text = await this.spellService.GetCorrectedTextAsync(activity.Text);
+                        ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                        var input = activity.Text;
+
+                        Task.Run(async () =>
+                        {
+                            var accessToken = await GetAuthenticationToken(ApiKey);
+                            var output = await TranslateText(input, targetLang, accessToken);
+                            Console.WriteLine(output);
+                            activity.Text = output;
+                            await Conversation.SendAsync(activity, () => new RootLuisDialog());
+                            // Activity reply = activity.CreateReply($"Your text translation to English is => '{output}'");
+                            //  await connector.Conversations.ReplyToActivityAsync(reply);
+
+                        }).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.TraceError(ex.ToString());
+                    }
+               // }
+
+                //await Conversation.SendAsync(activity, () => new RootLuisDialog());
+            }
+            else
+            {
+                this.HandleSystemMessage(activity);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+        static async Task<string> TranslateText(string inputText, string language, string accessToken)
+        {
+            string url = "http://api.microsofttranslator.com/v2/Http.svc/Translate";
+            string query = $"?text={System.Net.WebUtility.UrlEncode(inputText)}&to={language}&contentType=text/plain";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var response = await client.GetAsync(url + query);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                    return "Hata: " + result;
+
+                var translatedText = XElement.Parse(result).Value;
+                return translatedText;
+            }
+        }
+
+        static async Task<string> GetAuthenticationToken(string key)
+        {
+            string endpoint = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+                var response = await client.PostAsync(endpoint, null);
+                var token = await response.Content.ReadAsStringAsync();
+                return token;
+            }
+        }
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
